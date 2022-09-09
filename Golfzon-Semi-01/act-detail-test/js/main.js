9536439344d97cb6340f67246de6a94c2f95816f
Jason9789 <lifebook0809@gmail.com>
2022-09-09 17:35:50 +0900

    feat: 모임 상세 페이지 aside 구현

---
+++ Golfzon-Semi-01/act-detail-test/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  console.log('ready')
+  $('#plusImg').click(function () {
+    $('#fold_tag').toggleClass('blind')
+  })
+
+  $('.tagItem').click(function (e) {
+    $('.tagItem').removeClass('check')
+    $(e.target).addClass('check')
+  })
+})
