441a42cce7f5335953f76e6f717957932002fcaa
Jason9789 <lifebook0809@gmail.com>
2022-09-14 10:37:23 +0900

    fix: js to jquery convert

--- Golfzon-Semi-01/act-test/js/select-list.js
+++ Golfzon-Semi-01/act-test/js/select-list.js
@@ -1,27 +1,25 @@
-window.onload = () => {
-  const label = document.querySelector('.label')
+$(document).ready(function () {
+  const label = $('.label')
   const options = document.querySelectorAll('.option-item')
-
-  console.log(label)
   console.log(options)
 
-  // 클릭한 옵션의 text를 label에 넣기
-  const handleSelect = (item) => {
-    label.parentElement.classList.remove('active')
-    label.innerHTML = item.textContent
+  $.handleSelect = function (item) {
+    label.parent().removeClass('active')
+    label.html($(item.target).text())
   }
 
-  // 옵션 클릭시 클릭한 옵션 넘김
-  options.forEach((option) => {
-    option.addEventListener('click', () => handleSelect(option))
+  $.each(options, function (index) {
+    console.log(index)
+    $(this).click(function (element) {
+      $.handleSelect(element)
+    })
   })
 
-  // 라벨 클릭시 옵션 목록이 열림/닫힘
-  label.addEventListener('click', () => {
-    if (label.parentNode.classList.contains('active')) {
-      label.parentNode.classList.remove('active')
+  $('.label').click(function () {
+    if (label.parent().hasClass('active') === true) {
+      label.parent().removeClass('active')
     } else {
-      label.parentNode.classList.add('active')
+      label.parent().addClass('active')
     }
   })
-}
+})
