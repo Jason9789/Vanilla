9cd653c2c31aff664fb0fea61e136dd4e1e072b0
Jason9789 <lifebook0809@gmail.com>
2022-09-22 21:54:59 +0900

    add: custom selector

--- Golfzon-Semi-01/act-detail-test/js/select-list.js
+++ Golfzon-Semi-01/act-detail-test/js/select-list.js
@@ -1,27 +1,27 @@
 window.onload = () => {
-  const label = document.querySelector('.label')
-  const options = document.querySelectorAll('.option-item')
+  const img = document.querySelector('.img-more')
+  const options = document.querySelectorAll('.post-option-item')
 
-  console.log(label)
+  console.log(img)
   console.log(options)
 
   // 클릭한 옵션의 text를 label에 넣기
-  const handleSelect = (item) => {
-    label.parentElement.classList.remove('active')
-    label.innerHTML = item.textContent
-  }
+  // const handleSelect = (item) => {
+  //   img.parentElement.classList.remove('active')
+  //   label.innerHTML = item.textContent
+  // }
 
   // 옵션 클릭시 클릭한 옵션 넘김
-  options.forEach((option) => {
-    option.addEventListener('click', () => handleSelect(option))
-  })
+  // options.forEach((option) => {
+  //   option.addEventListener('click', () => handleSelect(option))
+  // })
 
   // 라벨 클릭시 옵션 목록이 열림/닫힘
-  label.addEventListener('click', () => {
-    if (label.parentNode.classList.contains('active')) {
-      label.parentNode.classList.remove('active')
+  img.addEventListener('click', () => {
+    if (img.parentNode.classList.contains('active')) {
+      img.parentNode.classList.remove('active')
     } else {
-      label.parentNode.classList.add('active')
+      img.parentNode.classList.add('active')
     }
   })
 }
