b198f075211aab3ea26f996b4fe20d8e8c1fb113
Jason9789 <lifebook0809@gmail.com>
2022-10-25 18:36:09 +0900

    feat: login popup

--- rence-markup/js/header.js
+++ rence-markup/js/header.js
@@ -15,6 +15,7 @@ $(function () {
     $('.popup-background:eq(0)').removeClass('blind')
     $('#login-section').removeClass('blind')
   })
+
   $('#go-join').click(function () {
     $('#before_login>.custom-select-host').addClass('blind')
     $('.popup-background:eq(0)').removeClass('blind')
@@ -30,41 +31,4 @@ $(function () {
     $('#logout-popup').removeClass('blind')
     $('.popup-background:eq(0)').removeClass('blind')
   })
-
-  // CUSTOM TYPE SELECT
-  $('#type').click(function () {
-    $('#custom-type-select').toggleClass('blind')
-  })
-  $('.type-select-list').click(function (e) {
-    $('#type>span').text($(this).text())
-    $('#custom-type-select').addClass('blind')
-  })
-
-  // CUSTOM LOCATION SELECT
-  $('#location').click(function () {
-    if (city.length == 0) {
-      $.ajax({
-        url: '../json/city.json',
-        dataType: 'json',
-        success: function (res) {
-          cityObject = res.slice()
-          console.log(cityObject)
-
-          city = cityObject
-            .map(function (v) {
-              if (v.city != null) return v.city
-            })
-            .slice()
-        },
-        error: function (err) {
-          console.log(err)
-        },
-      })
-    }
-    $('#custom-location-select').toggleClass('blind')
-  })
-
-  function load_city() {
-    $('.location-select-city-list.sample')
-  }
 })
