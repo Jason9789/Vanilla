737b6eea2f62395e71bd7eaec1a89574e566658e
Jason9789 <lifebook0809@gmail.com>
2022-10-26 21:39:13 +0900

    feat: 대시보드 일일 정산 요약 및 공간 요약 퍼블리싱

---
+++ rence-markup/js/number-comma.js
@@ -0,0 +1,37 @@
+$(function () {
+  $('.sales-income').text(
+    $('.sales-income')
+      .text()
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ','),
+  )
+
+  $('.sales-revenue').text(
+    $('.sales-revenue')
+      .text()
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ','),
+  )
+
+  $('.sales-cancel').text(
+    $('.sales-cancel')
+      .text()
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ','),
+  )
+
+  $('.reserve-count').text(
+    $('.reserve-count')
+      .text()
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ','),
+  )
+
+  $('.qna-count').text(
+    $('.qna-count')
+      .text()
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ','),
+  )
+
+  $('.review-count').text(
+    $('.review-count')
+      .text()
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ','),
+  )
+})
