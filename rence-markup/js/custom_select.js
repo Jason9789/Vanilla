60277cba8638f5c61ae2eac479f292c3fd1e0b77
Jason9789 <lifebook0809@gmail.com>
2022-10-27 20:47:31 +0900

    [refactor] popup 버그 수정

--- rence-markup/js/custom_select.js
+++ rence-markup/js/custom_select.js
@@ -1,23 +1,51 @@
 $(function () {
-  $('#btn-cancel').click(function () {
-    $('#room-insert-section').addClass('blind')
-    $('.popup-background').addClass('blind')
-  })
-
   $('#btn-room-add').click(function () {
     $('#room-insert-section').removeClass('blind')
     $('.popup-background').removeClass('blind')
   })
 
-  $('#room-type-label').on('click', function () {
-    $('.type-select-list').toggleClass('blind')
+  $('#btn-insert-cancel').click(function () {
+    $('#room-insert-section').addClass('blind')
+    $('.popup-background').addClass('blind')
   })
 
-  $('.type-select-item').on('click', function () {
+  $('#btn-edit-cancel').click(function () {
+    $('#room-edit-section').addClass('blind')
+    $('.popup-background').addClass('blind')
+  })
+
+  $('#btn-edit-cancel').click(function () {
+    $('#room-insert-section').addClass('blind')
+    $('.popup-background').addClass('blind')
+  })
+
+  $('.btn-room-edit').on('click', function () {
+    $('#room-edit-section').removeClass('blind')
+    $('.popup-background').removeClass('blind')
+  })
+
+  $('#insert-room-type-label').on('click', function () {
+    $('.insert-type-select-list').toggleClass('blind')
+  })
+
+  $('#edit-room-type-label').on('click', function () {
+    console.log('zzzz')
+    $('.edit-type-select-list').toggleClass('blind')
+  })
+
+  $('.insert-type-select-item').on('click', function () {
     let type = $(this).text()
     console.log(type)
-    $('#room-type-label').text(type)
-    $('#room-type-label').css('color', '#000000')
-    $('.type-select-list').addClass('blind')
+    $('#insert-room-type-label').text(type)
+    $('#insert-room-type-label').css('color', '#000000')
+    $('.insert-type-select-list').addClass('blind')
+  })
+
+  $('.edit-type-select-item').on('click', function () {
+    let type = $(this).text()
+    console.log(type)
+    $('#edit-room-type-label').text(type)
+    $('#edit-room-type-label').css('color', '#000000')
+    $('.edit-type-select-list').addClass('blind')
   })
 })
