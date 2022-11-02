3fa8585ef722c076f7762025541f8e950f9432c3
Jason9789 <lifebook0809@gmail.com>
2022-11-02 15:33:29 +0900

    refactor: file 수정

---
+++ rence-markup/js/host_insert.js
@@ -0,0 +1,116 @@
+/**
+ * @author : 전판근
+ */
+
+// ******************
+// 호스트 신청에 필요한 JS
+// ******************
+
+$(function () {
+  // TIME PICKER
+  $('.time-picker').timepicker({
+    timeFormat: 'HH:mm',
+    interval: 60,
+    defaultTime: '09',
+    dynamic: false,
+    dropdown: true,
+    scrollbar: true,
+  })
+
+  // 휴무일 체크시 timepicker block
+  $('#sun_dayoff').click(function () {
+    if (this.checked) {
+      $('#sun_stime').attr('disabled', true)
+      $('#sun_etime').attr('disabled', true)
+    } else {
+      $('#sun_stime').attr('disabled', false)
+      $('#sun_etime').attr('disabled', false)
+    }
+  })
+
+  $('#mon_dayoff').click(function () {
+    if (this.checked) {
+      $('#mon_stime').attr('disabled', true)
+      $('#mon_etime').attr('disabled', true)
+    } else {
+      $('#mon_stime').attr('disabled', false)
+      $('#mon_etime').attr('disabled', false)
+    }
+  })
+
+  $('#tue_dayoff').click(function () {
+    if (this.checked) {
+      $('#tue_stime').attr('disabled', true)
+      $('#tue_etime').attr('disabled', true)
+    } else {
+      $('#tue_stime').attr('disabled', false)
+      $('#tue_etime').attr('disabled', false)
+    }
+  })
+
+  $('#wed_dayoff').click(function () {
+    if (this.checked) {
+      $('#wed_stime').attr('disabled', true)
+      $('#wed_etime').attr('disabled', true)
+    } else {
+      $('#wed_stime').attr('disabled', false)
+      $('#wed_etime').attr('disabled', false)
+    }
+  })
+
+  $('#thu_dayoff').click(function () {
+    if (this.checked) {
+      $('#thu_stime').attr('disabled', true)
+      $('#thu_etime').attr('disabled', true)
+    } else {
+      $('#thu_stime').attr('disabled', false)
+      $('#thu_etime').attr('disabled', false)
+    }
+  })
+
+  $('#fri_dayoff').click(function () {
+    if (this.checked) {
+      $('#fri_stime').attr('disabled', true)
+      $('#fri_etime').attr('disabled', true)
+    } else {
+      $('#fri_stime').attr('disabled', false)
+      $('#fri_etime').attr('disabled', false)
+    }
+  })
+
+  $('#sat_dayoff').click(function () {
+    if (this.checked) {
+      $('#sat_stime').attr('disabled', true)
+      $('#sat_etime').attr('disabled', true)
+    } else {
+      $('#sat_stime').attr('disabled', false)
+      $('#sat_etime').attr('disabled', false)
+    }
+  })
+
+  // backoffice_type 필수 입력 체크
+  $('#submit-application').click(function () {
+    var result = true
+
+    var desk_checked = $('#type_checkbox_desk').is(':checked')
+    var meeting_room_checked = $('#type_checkbox_meeting_room').is(':checked')
+    var office_checked = $('#type_checkbox_office').is(':checked')
+
+    if (desk_checked || meeting_room_checked || office_checked) {
+      $('submit-application').submit()
+      result = true
+    } else {
+      $('.popup-background').removeClass('blind')
+      $('#fail-alert-popup').removeClass('blind')
+      result = false
+    }
+
+    return result
+  })
+
+  // 빈 항목 팝업 닫기
+  $('#empty-fail-alert-btn').click(function () {
+    $('#fail-alert-popup').addClass('blind')
+    $('.popup-background').addClass('blind')
+  })
+})
--- rence-markup/js/host_popup.js
+++ rence-markup/js/host_popup.js
@@ -21,4 +21,87 @@ $(function () {
     $('#logout-popup').addClass('blind')
     $('.popup-background:eq(0)').addClass('blind')
   })
+
+  $('#btn-room-add').click(function () {
+    $('#room-insert-section').removeClass('blind')
+    $('.popup-background').removeClass('blind')
+  })
+
+  $('#btn-insert-cancel').click(function () {
+    $('.room_type').val('')
+    $('#room-insert-section').addClass('blind')
+    $('.popup-background').addClass('blind')
+  })
+
+  $('#btn-edit-cancel').click(function () {
+    $('.room_type').val('')
+    $('#room-edit-section').addClass('blind')
+    $('.popup-background').addClass('blind')
+  })
+
+  $('.btn-room-edit').on('click', function () {
+    $('#room-edit-section').removeClass('blind')
+    $('.popup-background').removeClass('blind')
+  })
+
+  $('.btn-room-delete').on('click', function () {
+    $('#delete-popup').removeClass('blind')
+    $('.popup-background').removeClass('blind')
+  })
+
+  $('#insert-room-type-label').on('click', function () {
+    $('.insert-type-select-list').toggleClass('blind')
+  })
+
+  $('#edit-room-type-label').on('click', function () {
+    $('.edit-type-select-list').toggleClass('blind')
+  })
+
+  $('.insert-type-select-item').on('click', function () {
+    let type = $(this).text()
+    $('.room_type').val(type)
+    $('#insert-room-type-label').text(type)
+    $('#insert-room-type-label').css('color', '#000000')
+    $('.insert-type-select-list').addClass('blind')
+  })
+
+  $('.edit-type-select-item').on('click', function () {
+    let type = $(this).text()
+    $('.room_type').val(type)
+    $('#edit-room-type-label').text(type)
+    $('#edit-room-type-label').css('color', '#000000')
+    $('.edit-type-select-list').addClass('blind')
+  })
+
+  // 리뷰 답글 작성
+  $('.review-add').on('click', function () {
+    $('#comment-section').removeClass('blind')
+    $('.popup-background').removeClass('blind')
+  })
+
+  $('.btn-comment-cancel').on('click', function () {
+    $('#comment-section').addClass('blind')
+    $('.popup-background').addClass('blind')
+  })
+
+  // 환경 설정 부분 팝업 관련
+  $('#btn-update-pw').on('click', function () {
+    $('#popup-update-pw').removeClass('blind')
+    $('.popup-background').removeClass('blind')
+  })
+
+  $('#btn-popup-close').on('click', function () {
+    $('#popup-update-pw').addClass('blind')
+    $('.popup-background').addClass('blind')
+  })
+
+  $('#btn-host-delete').on('click', function () {
+    $('#host-delete-popup').removeClass('blind')
+    $('.popup-background').removeClass('blind')
+  })
+
+  $('#delete-closeBtn').on('click', function () {
+    $('#host-delete-popup').addClass('blind')
+    $('.popup-background').addClass('blind')
+  })
 })
